4d3622015e2e4a62fcea3702a809ad87d9c3634b
Federico Brid <federicobrid@gmai.com>
2022-03-31 00:46:26 -0300

    portfolio in process

---
+++ main.js
@@ -0,0 +1,13 @@
+// Swiper Slide Projects
+let swiper = new Swiper(".projects-container", {
+    cssMode: true,
+    loop:true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});

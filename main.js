2a30bdf97794e9d62ca07e04e3fadd7c37057d61
Federico Brid <federicobrid@gmai.com>
2022-04-03 20:02:40 -0300

    portfolio unfinished

--- main.js
+++ main.js
@@ -11,3 +11,25 @@ let swiper = new Swiper(".projects-container", {
       clickable: true,
     },
 });
+
+// Email JS
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_hti28rn';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
